604ff740be2607ee241646a8d7202cb48bc0bf97
Sumit Kumar <sumitsingh1999220@gmail.com>
2025-11-17 17:47:15 +0530

    Add files via upload

---
+++ End_Sem_Practical/final_practical_1.java
@@ -0,0 +1,20 @@
+class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+    double add(double a, double b) {
+        return a + b;
+    }
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+class final_practical_1 {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        System.out.println(c.add(5, 10));        
+        System.out.println(c.add(5.5, 2.5));    
+        System.out.println(c.add(1, 2, 3));    
+    }
+}
---
+++ End_Sem_Practical/final_practical_3.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class InvalidAgeException extends Exception {
+    
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class final_practical_3 {
+    
+    static void checkAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            
+            throw new InvalidAgeException("Invalid Age: You must be 18 or older.");
+        } else {
+            System.out.println("Valid Age: You are eligible!");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        try {
+            
+            checkAge(age);
+        } catch (InvalidAgeException e) {
+            
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+
+        sc.close();
+    }
+}
